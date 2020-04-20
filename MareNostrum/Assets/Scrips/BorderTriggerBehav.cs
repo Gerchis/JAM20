945627137d6ad8b030abb58a7957c08ce8d876ac
Gerchis <rogerba97@gmail.com>
2020-04-20 13:44:50 +0200

    PrefabFolder

---
+++ MareNostrum/Assets/Scrips/BorderTriggerBehav.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderTriggerBehav : MonoBehaviour
+{
+    public bool col = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            col = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            col = true;
+        }
+    }
+}
