5018c10b9a6b314e17761cf468d57246ff249c26
Gerchis <rogerba97@gmail.com>
2020-04-20 15:10:30 +0200

    TextMeshPro

---
+++ MareNostrum/Assets/Scrips/PlayerBehav.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehav : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb.GetComponent<Rigidbody2D>();   
+    }
+        
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (InputManager.Instance.upKey)
+        {
+            //rb.AddForce(Vector2.up, ForceMode.Force);
+        }
+        else if (InputManager.Instance.downKey)
+        {
+
+        }
+
+        if (InputManager.Instance.rightKey && !InputManager.Instance.leftKey)
+        {
+
+        }
+        else if (InputManager.Instance.leftKey && !InputManager.Instance.rightKey)
+        {
+
+        }
+
+    }
+}
