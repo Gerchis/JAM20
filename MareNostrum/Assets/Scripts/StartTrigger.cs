e5623fce265a143b125e3e771b5d4d071cee7e7e
Sergio Murillo Mendez <sergio.murillo.mendez@gmail.com>
2020-04-21 23:37:31 +0200

    merge

--- MareNostrum/Assets/Scripts/StartTrigger.cs
+++ MareNostrum/Assets/Scripts/StartTrigger.cs
@@ -1,16 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartTrigger : MonoBehaviour
 {
+    private Slider slider;
+    private bool RegenerateEnergy;
+
+    private float actTime;
+    private float maxTime;
+
+
     //GameObject padre que agrupa todos los peces del nivel
     private GameObject fishes;
+
     
     void Start()
     {
+        slider = GameObject.Find("EnergySlider").GetComponent<Slider>();
+
         fishes = GameObject.Find("Fishes");
         GetComponent<SpriteRenderer>().enabled = false;
+
+        maxTime = 25;
+    }
+
+    private void FixedUpdate()
+    {
+        if (RegenerateEnergy)
+        {
+            actTime += Time.fixedDeltaTime * 1000;
+
+            if (slider.value < 100)
+            {
+                if (actTime >= maxTime)
+                {
+                    actTime = 0;
+                    slider.value += 1;
+                }
+            }
+            else
+            {
+                RegenerateEnergy = false;
+            }
+        }
     }
 
     //Cuando el Player entra al mar, respawnean peces y se desactiva el consumir energia.
@@ -18,15 +52,21 @@ public class StartTrigger : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            GameManager.Instance.consumeEnergy = false;
-            GameManager.Instance.playerControl = true;
-            GameManager.Instance.RestoreEnergy();
-
             foreach (Transform child in fishes.transform)
             {
                 child.GetComponent<FishController>().EnableFish();
-
             }
+        }
+    }
+
+    
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            RegenerateEnergy = true;
+            GameManager.Instance.consumeEnergy = false;
+            GameManager.Instance.playerControl = true;
         }
     }
 
@@ -36,6 +76,7 @@ public class StartTrigger : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             GameManager.Instance.consumeEnergy = true;
+            RegenerateEnergy = false;
         }
     }
 }
