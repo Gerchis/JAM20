ba42142df774fb35a618375386af285fb577a2c1
Sergio Murillo Mendez <sergio.murillo.mendez@gmail.com>
2020-04-22 10:05:04 +0200

    splashScreen

--- MareNostrum/Assets/Scripts/SplasScriptController.cs
+++ MareNostrum/Assets/Scripts/SplasScriptController.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 using UnityEngine.SceneManagement;
 
 public class SplasScriptController : MonoBehaviour
 {
+    private VideoPlayer videoPlayer;
+
+    private void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.Play();
+    }
+
     void Update()
     {
 
-        //TODO: Para splash screen video if (videoPlayer.time > 0 && !videoPlayer.isPlaying || Input.anyKey)
-        if (Input.anyKey)
+        if (videoPlayer.time > 0 && !videoPlayer.isPlaying || Input.anyKey)
         {
             SceneManager.LoadScene(1);
         }
